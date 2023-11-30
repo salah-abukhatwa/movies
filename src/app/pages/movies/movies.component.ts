22f4b7b5aaf116d06138cb26c06a9615cbdc4ed0
salah-abukhatwa <salah.abukhatwa@gmail.com>
2023-11-30 15:14:27 +0300

    creat movies page

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Movie } from "../../models/movie.model";
+import { MoviesService } from "src/app/services/movies.service";
 
 @Component({
-  selector: 'app-movies',
-  templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+    selector: "app-movies",
+    templateUrl: "./movies.component.html",
+    styleUrls: ["./movies.component.scss"]
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
+    movies: Movie[] = [];
 
+    constructor(private movieService: MoviesService) {}
+
+    ngOnInit(): void {
+        this.movieService.searchMovies(2).subscribe((movies) => {
+            this.movies = movies;
+        });
+    }
 }
