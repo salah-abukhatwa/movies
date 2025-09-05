91a87204b339da522e9171210d0af1c2cf92f0af
salah-abukhatwa <roben.gn2@gmail.com>
2025-09-05 16:50:10 +0300

    ui: improved card animations and hover effects for home page

--- src/app/components/items-banner/items-banner.component.ts
+++ src/app/components/items-banner/items-banner.component.ts
@@ -1,14 +1,45 @@
 import { Component, Input } from "@angular/core";
-import { Movie } from "../../models/movie.model";
+import {
+    trigger,
+    transition,
+    style,
+    animate,
+    query,
+    stagger
+} from "@angular/animations";
+
+import { Movie } from "src/app/models/movie.model";
 import { TvShow } from "src/app/models/tvShow.model";
 
 @Component({
     selector: "app-items-banner",
     templateUrl: "./items-banner.component.html",
-    styleUrls: ["./items-banner.component.scss"]
+    styleUrls: ["./items-banner.component.scss"],
+    animations: [
+        trigger("fadeInStagger", [
+            transition(":enter", [
+                query(
+                    ":enter",
+                    [
+                        style({ opacity: 0, transform: "translateY(30px)" }),
+                        stagger("200ms", [
+                            animate(
+                                "1000ms ease-in-out",
+                                style({
+                                    opacity: 1,
+                                    transform: "translateY(0)"
+                                })
+                            )
+                        ])
+                    ],
+                    { optional: true }
+                )
+            ])
+        ])
+    ]
 })
 export class ItemsBannerComponent {
     @Input() items: Movie[] = [];
     @Input() itemstv: TvShow[] = [];
-    @Input() title: string = "";
+    @Input() title = "";
 }
