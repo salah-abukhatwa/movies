d85d1fb6da6775ddb243a0dafe5122210eda1e7c
salah-abukhatwa <roben.gn2@gmail.com>
2025-09-05 13:59:11 +0300

    enhance header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: "app-header",
+    templateUrl: "./header.component.html",
+    styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent {
+    isMenuOpen = false;
 
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
+    closeMenu() {
+        this.isMenuOpen = false;
+    }
 }
